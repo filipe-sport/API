d4476103e692a0f9c6d13b311ae653e0ce3b6337
Filipe <filipecesarufpe@gmail.com>
2022-12-09 23:14:03 -0300

    update files

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-function createNode(element) {
-    return document.createElement(element);
-}
+ 
+    const btn = document.querySelector('#send')
+    btn.addEventListener("click", function dataSend(e){
+      
+      e.preventDefault();
+      const apresentar = document.querySelector("#apresentar")
+      const name = document.querySelector("#name");
+      const ul = document.getElementById('nascidos');
+      formulario.innerHTML = ''
+      const value = name.value;
+      apresentar.innerHTML = value
+      
+      let  url = `https://servicodados.ibge.gov.br/api/v2/censos/nomes/${value}`;
+      
+      fetch(url)
+      .then((resp) => resp.json())
+      .then(function(data) {
 
-function append(parent, el) {
-  return parent.appendChild(el);
-}
-
-const ul = document.getElementById('authors');
-const url = 'https://randomuser.me/api/?results=10';
-
-fetch(url)
-.then((resp) => resp.json())
-.then(function(data) {
-  let authors = data.results;
-  return authors.map(function(author) {
-    let li = createNode('li');
-    let img = createNode('img');
-    let span = createNode('span');
-    img.src = author.picture.medium;
-    span.innerHTML = `${author.name.first} ${author.name.last}`;
-    append(li, img);
-    append(li, span);
-    append(ul, li);
-  })
+      let i = 0
+      
+        while (i< data[0].res.length){
+          
+      
+          let authors = data[0].res[i]
+          let periodo = authors.periodo
+          let frequencia = authors.frequencia
+          let li = document.createElement('li')
+          li.innerHTML = (periodo +" nascidos: "+ frequencia+" ]");
+          
+          ul.appendChild(li)
+          i++
+        }
+    })
+    
 })
 .catch(function(error) {
   console.log(error);
 });
-  
